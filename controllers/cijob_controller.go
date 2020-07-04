8884987d8721c9b96dd5e60b80dfc1682f6922af
Erik Hollensbe <github@hollensbe.org>
2020-07-04 06:57:50 +0000

    Several refactors:
    - moved build and delete routines to separate functions (out of
    reconciler)
    - normalized logging a bit
    
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- controllers/cijob_controller.go
+++ controllers/cijob_controller.go
@@ -17,6 +17,7 @@ import (
 )
 
 var defaultResult = ctrl.Result{}
+var requeueResult = ctrl.Result{Requeue: true}
 
 // CIJobReconciler reconciles a CIJob object
 type CIJobReconciler struct {
@@ -42,6 +43,10 @@ func (r *CIJobReconciler) getPod(ctx context.Context, req ctrl.Request) (*corev1
 
 func (r *CIJobReconciler) getPodLogger(req ctrl.Request) logr.Logger {
 	return r.Log.WithValues("cijob", req.NamespacedName, "pod", getPodName(req))
+}
+
+func (r *CIJobReconciler) getJobLogger(req ctrl.Request) logr.Logger {
+	return r.Log.WithValues("cijob", req.NamespacedName)
 }
 
 func getName(req ctrl.Request, append string) types.NamespacedName {
@@ -78,7 +83,7 @@ func (r *CIJobReconciler) supervisePod(ctx context.Context, req ctrl.Request) {
 
 		pod, err := r.getPod(ctx, req)
 		if err != nil {
-			podLog.Info("error while retrieving ci job pod", "error", err.Error())
+			podLog.Error(err, "error while retrieving ci job pod")
 			errCount++
 			continue
 		}
@@ -87,7 +92,7 @@ func (r *CIJobReconciler) supervisePod(ctx context.Context, req ctrl.Request) {
 		if pod.Status.Phase != corev1.PodPending && state != nil {
 			cijob := &objectsv1alpha1.CIJob{}
 			if err := r.Get(ctx, req.NamespacedName, cijob); err != nil {
-				podLog.Info("pod is finished; could not retrieve CI job", "error", err.Error())
+				podLog.Error(err, "pod is finished; could not retrieve CI job")
 				errCount++
 				continue
 			}
@@ -96,7 +101,7 @@ func (r *CIJobReconciler) supervisePod(ctx context.Context, req ctrl.Request) {
 			cijob.Status.Success = state.ExitCode == 0
 
 			if err := r.Update(ctx, cijob); err != nil {
-				podLog.Info("error updating cijob with run state", "error", err.Error())
+				podLog.Error(err, "error updating cijob with run state")
 				errCount++
 				continue
 			}
@@ -124,6 +129,95 @@ func (r *CIJobReconciler) safeCreate(ctx context.Context, obj runtime.Object) er
 	return r.Client.Create(ctx, obj)
 }
 
+func (r *CIJobReconciler) waitForRepository(ctx context.Context, gn types.NamespacedName) (*sourcev1alpha1.GitRepository, error) {
+	var fetchedRepo *sourcev1alpha1.GitRepository
+
+	for {
+		select {
+		case <-ctx.Done():
+			// FIXME probably need to cleanup here
+			return nil, ctx.Err()
+		default:
+		}
+
+		fetchedRepo = &sourcev1alpha1.GitRepository{}
+
+		if err := r.Client.Get(ctx, gn, fetchedRepo); client.IgnoreNotFound(err) != nil {
+			return nil, err
+		}
+
+		if fetchedRepo.Status.Artifact != nil && fetchedRepo.Status.Artifact.URL != "" {
+			r.Log.Info("fetched repository, moving forward with create", "artifact", fetchedRepo.Status.Artifact.URL)
+			break
+		}
+	}
+
+	return fetchedRepo.DeepCopy(), nil
+}
+
+func (r *CIJobReconciler) removeJob(ctx context.Context, req ctrl.Request) error {
+	log := r.getJobLogger(req)
+
+	log.Info("cijob removed")
+
+	toDelete := []deleteItem{
+		{
+			logName: "git repository",
+			nsName:  getGitName(req),
+			obj:     &sourcev1alpha1.GitRepository{},
+		},
+		{
+			logName: "git secrets",
+			nsName:  getSecretName(req),
+			obj:     &corev1.Secret{},
+		},
+		{
+			logName: "pod",
+			nsName:  getPodName(req),
+			obj:     &corev1.Pod{},
+		},
+	}
+
+	for _, del := range toDelete {
+		log.Info("deleting resource", "type", del.logName, "name", del.nsName)
+		if err := r.safeDelete(ctx, del.nsName, del.obj); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (r *CIJobReconciler) buildJob(ctx context.Context, req ctrl.Request, cijob *objectsv1alpha1.CIJob) error {
+	gn := getGitName(req)
+	sn := getSecretName(req)
+
+	if err := r.safeCreate(ctx, cijob.Secret(sn)); err != nil {
+		return err
+	}
+
+	if err := r.safeCreate(ctx, cijob.GitRepository(gn, sn.Name)); err != nil {
+		return err
+	}
+
+	_, err := r.waitForRepository(ctx, gn)
+	if err != nil {
+		return err
+	}
+
+	// FIXME sew artifact into container image
+
+	if err := r.Client.Create(ctx, cijob.Pod(getPodName(req))); err != nil {
+		return err
+	}
+
+	go r.supervisePod(ctx, req)
+
+	// only the name can be used here, otherwise badness in the runner. We already know the namespace.
+	cijob.Status.PodName = getPodName(req).Name
+	return r.Update(ctx, cijob)
+}
+
 type deleteItem struct {
 	logName string
 	nsName  types.NamespacedName
@@ -136,90 +230,24 @@ type deleteItem struct {
 // Reconcile the resource
 func (r *CIJobReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	ctx := context.Background()
-	log := r.Log.WithValues("cijob", req.NamespacedName)
+	log := r.getJobLogger(req)
 
 	cijob := &objectsv1alpha1.CIJob{}
 
 	if err := r.Get(ctx, req.NamespacedName, cijob); err != nil {
-		log.Info("cijob removed")
-
-		toDelete := []deleteItem{
-			{
-				logName: "git repository",
-				nsName:  getGitName(req),
-				obj:     &sourcev1alpha1.GitRepository{},
-			},
-			{
-				logName: "git secrets",
-				nsName:  getSecretName(req),
-				obj:     &corev1.Secret{},
-			},
-			{
-				logName: "pod",
-				nsName:  getPodName(req),
-				obj:     &corev1.Pod{},
-			},
-		}
-
-		for _, del := range toDelete {
-			r.Log.Info("deleting resource", "type", del.logName)
-			if err := r.safeDelete(ctx, del.nsName, del.obj); err != nil {
-				return defaultResult, err
-			}
-		}
-
-		return defaultResult, nil
+		return defaultResult, r.removeJob(ctx, req)
 	}
 
 	if err := cijob.Validate(); err != nil {
-		r.Log.Error(err, "encountered cijob validation error")
+		log.Error(err, "encountered cijob validation error")
 		return defaultResult, err
 	}
 
 	_, err := r.getPod(ctx, req)
-	if err != nil {
-		gn := getGitName(req)
-		sn := getSecretName(req)
-
-		if err := r.safeCreate(ctx, cijob.Secret(sn)); err != nil {
-			return defaultResult, err
-		}
-
-		if err := r.safeCreate(ctx, cijob.GitRepository(gn, sn.Name)); err != nil {
-			return defaultResult, err
-		}
-
-		for {
-			select {
-			case <-ctx.Done():
-				// FIXME probably need to cleanup here
-				return defaultResult, ctx.Err()
-			default:
-			}
-
-			fetchedRepo := &sourcev1alpha1.GitRepository{}
-
-			if err := r.Client.Get(ctx, gn, fetchedRepo); client.IgnoreNotFound(err) != nil {
-				return defaultResult, err
-			}
-
-			if fetchedRepo.Status.Artifact != nil && fetchedRepo.Status.Artifact.URL != "" {
-				r.Log.Info("fetched repository, moving forward with create", "artifact", fetchedRepo.Status.Artifact.URL)
-				break
-			}
-		}
-
-		// FIXME sew artifact into container image
-
-		if err := r.Client.Create(ctx, cijob.Pod(getPodName(req))); err != nil {
-			return defaultResult, err
-		}
-
-		go r.supervisePod(ctx, req)
-
-		// only the name can be used here, otherwise badness in the runner. We already know the namespace.
-		cijob.Status.PodName = getPodName(req).Name
-		return defaultResult, r.Update(ctx, cijob)
+	if apierrors.IsNotFound(err) {
+		return defaultResult, r.buildJob(ctx, req, cijob)
+	} else if err != nil {
+		return requeueResult, err
 	}
 
 	return defaultResult, nil
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/go-logr/logr v0.1.0
 	github.com/onsi/ginkgo v1.11.0
 	github.com/onsi/gomega v1.8.1
+	github.com/prometheus/common v0.4.1
 	go.uber.org/atomic v1.5.1 // indirect
 	golang.org/x/lint v0.0.0-20191125180803-fdd1cda4f05f // indirect
 	golang.org/x/oauth2 v0.0.0-20191202225959-858c2ad4c8b6 // indirect
